42675a907ad8782e8fd73ca9026ada7e507997cc
CharankumarSarlani <charankumarsarlani1@gmail.com>
2024-11-16 23:58:46 +0530

    Update is_even_in_range.js

--- is_even_in_range.js
+++ is_even_in_range.js
@@ -22,4 +22,10 @@ function printEvens(start,end) {
   return printEvensInRange(closestEven(start), end);
 }
 
-console.log(printEvens(4,4));
+console.log(printEvens(4, 4));
+console.log(printEvens(2, 9));
+console.log(printEvens(2, 8));
+console.log(printEvens(3, 7));
+console.log(printEvens(3, 3));
+console.log(printEvens(3, 4));
+
