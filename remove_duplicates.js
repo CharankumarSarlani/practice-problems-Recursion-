f4ea386aa0fd1705be2a5460dd0499628387fe81
CharankumarSarlani <charankumarsarlani1@gmail.com>
2024-11-16 23:43:08 +0530

    Add files via upload

---
+++ remove_duplicates.js
@@ -0,0 +1,34 @@
+function __removeDuplicates(string, start) {
+  if (start > string.length) {
+    return '';
+  }
+
+  if (string[start] === string[start + 1]) {
+    return __removeDuplicates(string, start + 1);
+  }
+
+  return string[start] + __removeDuplicates(string, start + 1);
+}
+
+
+function removeDuplicates(string) {
+  return __removeDuplicates(string, 0);
+}
+
+// test fragment starts from here
+function testRemoveDuplicate(string, expected) {
+  const result = removeDuplicates(string);
+  console.log(result === expected ? '✅' : '❌', "|" + result + "|", "|" + 
+    expected + "|");
+}
+
+function testAll() {
+  testRemoveDuplicate("cccc", "c");
+  testRemoveDuplicate(" ", " ");
+  testRemoveDuplicate("cccs", "cs");
+  testRemoveDuplicate("cad", "cad");
+  testRemoveDuplicate("", "");
+}
+
+testAll();
+
