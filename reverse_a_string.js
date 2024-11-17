7dcde99333f37132e2a19b88cfbd058d7fe97c0f
CharankumarSarlani <charankumarsarlani1@gmail.com>
2024-11-17 09:06:07 +0530

    Update reverse_a_string.js

--- reverse_a_string.js
+++ reverse_a_string.js
@@ -1,16 +1,16 @@
 function _reverseString(string, start) {
-  if(string.length <= start) {
+  if (string.length < start) {
     return '';
   }
 
-  return string[string.length - ++start] + _reverseString(string, start);
+  return string[string.length - start] + _reverseString(string, start + 1);
 }
 
 function reverseString(string) {
-  return _reverseString(string, 0);
+  return _reverseString(string, 1);
 }
 
 console.log("|" + reverseString("charan") + "|");
-console.log("|" + reverseString("c")+ "|");
-console.log("|" + reverseString(" ")+ "|");
+console.log("|" + reverseString("c") + "|");
+console.log("|" + reverseString(" ") + "|");
 console.log("|" + reverseString("") + "|");
