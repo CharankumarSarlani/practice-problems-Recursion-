c7628573a9beee381de47e5a118b95de5aaa10b9
CharankumarSarlani <charankumarsarlani1@gmail.com>
2024-11-17 14:33:18 +0530

    Update hcf.js

--- hcf.js
+++ hcf.js
@@ -1,9 +1,17 @@
+function getRemainder(divisor, dividend) {
+  if (dividend < divisor) {
+    return dividend;
+  }
+
+  return getRemainder(divisor, dividend - divisor);
+}
+
 function hcf(a, b) {
   if (a === 0) {
     return b;
   }
 
-  return hcf(b % a, a);
+  return hcf(getRemainder(a, b), a);
 }
 
 console.log(hcf(0, 0));
@@ -11,3 +19,4 @@ console.log(hcf(2, 0));
 console.log(hcf(2, 2));
 console.log(hcf(2, 6));
 console.log(hcf(15, 30));
+console.log(hcf( 3, 5));
