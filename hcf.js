16ea2450654ce9d50001dc22e46e8e2d2a2f8ac3
CharankumarSarlani <charankumarsarlani1@gmail.com>
2024-11-17 14:07:19 +0530

    Update hcf.js

--- hcf.js
+++ hcf.js
@@ -6,6 +6,8 @@ function hcf(a, b) {
   return hcf(b % a, a);
 }
 
-console.log(hcf(2,0));
-console.log(hcf(2,2));
-console.log(hcf(15,30));
+console.log(hcf(0, 0));
+console.log(hcf(2, 0));
+console.log(hcf(2, 2));
+console.log(hcf(2, 6));
+console.log(hcf(15, 30));
